90d679fac55ced8f56972789cd279428b68ba4db
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2015-07-26 21:46:05 -0400

    Initial commit

---
+++ src/resiz/handler.clj
@@ -0,0 +1,29 @@
+(ns resiz.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [image-resizer.resize :refer :all]
+            [image-resizer.format :as format]
+            [image-resizer.scale-methods :refer :all]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :as response]))
+
+(defn resize-image
+  [image]
+
+  (def img (-> image
+               (io/resource)
+               (.getPath)
+               (io/file)))
+
+  (format/as-stream ((resize-fn 500 500 speed) img) "jpg"))
+
+(defroutes app-routes
+  (GET "/" []
+    (def resized-image (resize-image "madagascar-morondava.jpeg"))
+    (println resized-image)
+    (response/response resized-image))
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))

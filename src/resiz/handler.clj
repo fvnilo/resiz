e425437b309c950357cc9cbf058155d12ddd7a0f
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2015-07-28 22:08:10 -0400

    Much cleaner code

--- src/resiz/handler.clj
+++ src/resiz/handler.clj
@@ -8,30 +8,32 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.util.response :as response]))
 
-(defn resize-image
-  [filename height width]
+(defn number-string? [number-string]
+  (integer? (read-string number-string)))
 
-  (def image-file (-> filename
-                     (io/resource)
-                     (.getPath)
-                     (io/file)))
+(defn valid? [& number-strings]
+  (every? number-string? number-strings))
 
-  (format/as-stream ((resize-fn height width speed) image-file) "jpg"))
+(defn resize-image [path height width]
+  (let [resizer (resize-fn height width speed)
+        img-file (-> path (io/resource) (.getPath) (io/file))]
+        (format/as-stream (resizer img-file) "jpg")))
+
+(defn resize-handler [h w]
+    (if (not (valid? h w))
+      (response/status (response/response "invalid parameters") 400)
+      (let [height (Integer. h)
+            width   (Integer. w)
+            image-file "madagascar-morondava.jpeg"]
+
+        (with-open [image-stream (resize-image image-file height width)]
+          (-> image-stream
+              (response/response)
+              (response/content-type "image/jpeg"))))))    
 
 (defroutes resize-image-routes
-  (GET "/:height/:width" [height width]
-    (if (not (number? (read-string height)))
-      (response/status (response/response "invalid height") 400)
-      (if (not (number? (read-string width)))
-        (response/status (response/response "invalid width") 400)
-        (let [image-file "madagascar-morondava.jpeg"
-              height (read-string height)
-              width (read-string width)]
-          (-> (resize-image image-file height width)
-              (response/response)
-              (response/content-type "image/jpeg"))))))
-
-  (route/not-found "Not Found"))
+   (GET "/:height/:width" [height width] (resize-handler height width))
+   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults resize-image-routes site-defaults))
--- test/resiz/handler_test.clj
+++ test/resiz/handler_test.clj
@@ -3,8 +3,22 @@
             [ring.mock.request :as mock]
             [resiz.handler :refer :all]))
 
-(deftest test-app
-  (testing "image resize route"
+(deftest validations
+  (testing "the number-string parter"
+    (is (true? (number-string? "23")))
+    (is (false? (number-string? "foo"))))
+
+  (testing "the valid? function"
+    (is (true? (valid? "1" "2"))
+    (is (false? (valid? "1" "a"))
+    (is (false? (valid? "a" "1")))))))
+
+(deftest resizer
+  (testing "the resizer"
+    (is (not (nil? (resize-image "madagascar-morondava.jpeg" 1 1))))))
+
+(deftest routes
+  (testing "the image resize route"
     (let [response (app (mock/request :get "/1/1"))]
       (is (= (:status response) 200)))
     (let [response (app (mock/request :get "/a/1"))]
@@ -12,6 +26,6 @@
     (let [response (app (mock/request :get "/1/a"))]
       (is (= (:status response) 400))))
 
-  (testing "not-found route"
+  (testing "the not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
